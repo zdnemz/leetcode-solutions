6384ba8bb8d8a84cf48b8ed41c8a5205d8844610
zdnemz <zidanemz69@gmail.com>
2024-06-14 14:49:42 +0800

    add longestCommonPrefix solution

---
+++ src/easy/longestCommonPrefix.ts
@@ -0,0 +1,19 @@
+function longestCommonPrefix(strs: string[]): string {
+  if (strs.length === 0) return "";
+
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (prefix === "") return "";
+    }
+  }
+
+  return prefix;
+}
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
+console.log(longestCommonPrefix(["dog", "racecar", "car"]));
+
+// https://leetcode.com/problems/longest-common-prefix/
