cb302a1456865420341d8a11d9e9059e309b66d7
zdnemz <zidanemz69@gmail.com>
2024-06-16 13:34:00 +0800

    add removeDuplicatesFromSortedArray solution

---
+++ src/easy/removeDuplicatesFromSortedArray.ts
@@ -0,0 +1,19 @@
+function removeDuplicates(nums: number[]): number {
+  if (!nums) return 0;
+
+  let k: number = 1;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[i - 1]) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+}
+
+console.log(removeDuplicates([1, 1, 2]));
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
+
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/
