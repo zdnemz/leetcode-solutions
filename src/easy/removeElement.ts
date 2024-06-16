f22c4dafd3e8581d2697714437ad223a0b0188ec
zdnemz <zidanemz69@gmail.com>
2024-06-16 13:51:49 +0800

    add removeElement solution

---
+++ src/easy/removeElement.ts
@@ -0,0 +1,17 @@
+function removeElement(nums: number[], val: number): number {
+  let k: number = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[k] = nums[i];
+      k++;
+    }
+  }
+
+  return k;
+}
+
+console.log(removeElement([3, 2, 2, 3], 3));
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
+
+// https://leetcode.com/problems/remove-element/
